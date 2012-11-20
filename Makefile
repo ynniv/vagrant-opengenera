0435ed80050339322318cafaeb5341311ce19bce
kevin birch <kmb@pobox.com>
2012-11-19 19:35:41 -0500

    update postinstall to ensure linux sources are installed before guest additions are built; remove veewee box definition clobbering

--- Makefile
+++ Makefile
@@ -1,39 +1,38 @@
 # ubuntu requires packages git make rubygems libxml2-dev libxslt-dev zlib1g-dev virtualbox
 
-all: ubuntu-8.04.4-server-amd64 opengenera2.tar.bz2
+all: opengenera-ubuntu-8.04.4 opengenera2.tar.bz2
 
 clean:
-        vagrant destroy
-        rm -f ubuntu-8.04.4-server-amd64.box
+	vagrant destroy
+	rm -f opengenera-ubuntu-8.04.4.box
 
 # vagrant is a tool for automating virtualbox
 vagrant:
-        if test -z "$$(which vagrant)" ; then \
-          echo "### need to install vagrant"; \
-          sudo gem install vagrant; \
-        fi
+	if test -z "$$(which vagrant)" ; then \
+	  echo "### need to install vagrant"; \
+	  sudo gem install vagrant; \
+	fi
 
 # veewee is a vagrant addon that stages base operating system images
 veewee: vagrant
-        if test -z "$$(bash -c 'vagrant basebox' 2>&1 | grep 'vagrant basebox <command> ')" ; then \
-          echo "### need to install veewee"; \
-          sudo gem install veewee; \
-        fi
+	if test -z "$$(bash -c 'vagrant basebox' 2>&1 | grep 'vagrant basebox <command> ')" ; then \
+	  echo "### need to install veewee"; \
+	  sudo gem install veewee; \
+	fi
 
 # veewee might download an OS image. don't let make auto delete it, it's big!
 .PRECIOUS: %.iso
 
 # this is the base box we will build on. you should just download it if possible
-ubuntu-8.04.4-server-amd64.box: veewee
-        echo "### need to build ubuntu-8.04.4-server-amd64.box"
-        vagrant basebox define -f ubuntu-8.04.4-server-amd64 ubuntu-8.04.4-server-amd64;
-        vagrant basebox build  -f ubuntu-8.04.4-server-amd64;
-        vagrant basebox validate ubuntu-8.04.4-server-amd64;
-        vagrant basebox export   ubuntu-8.04.4-server-amd64;
+opengenera-ubuntu-8.04.4.box: veewee
+	echo "### need to build opengenera-ubuntu-8.04.4.box"
+	vagrant basebox build  --force opengenera-ubuntu-8.04.4;
+	vagrant basebox validate opengenera-ubuntu-8.04.4;
+	vagrant basebox export   opengenera-ubuntu-8.04.4;
 
 # test that the base box is installed. make it if not.
-ubuntu-8.04.4-server-amd64: vagrant
-        if test -z "$$(vagrant box list | grep -w ubuntu-8.04.4-server-amd64)" ; then \
-          make ubuntu-8.04.4-server-amd64.box; \
-          vagrant box add ubuntu-8.04.4-server-amd64 ubuntu-8.04.4-server-amd64.box; \
-        fi
+opengenera-ubuntu-8.04.4: vagrant
+	if test -z "$$(vagrant box list | grep -w opengenera-ubuntu-8.04.4)" ; then \
+	  make opengenera-ubuntu-8.04.4.box; \
+	  vagrant box add opengenera-ubuntu-8.04.4 opengenera-ubuntu-8.04.4.box; \
+	fi
--- definitions/ubuntu-8.04.4-server-amd64/postinstall.sh
+++ definitions/ubuntu-8.04.4-server-amd64/postinstall.sh
@@ -2,49 +2,61 @@
 
 date > /etc/vagrant_box_build_time
 
+#Installing the virtualbox guest additions
+echo "Installing the virtualbox guest additions"
+apt-get -y install dkms linux-kernel-devel linux-source linux-headers-$(uname -r) build-essential
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+echo "completed virtualbox guest additions"
+
 #Updating the box
+echo "Updating the box"
 apt-get -y update
-apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline5-dev nfs-common
 apt-get clean
+echo "completed updating the box"
 
 #Setting up sudo
+echo "setting up sudo"
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+echo "completed setting up sudo"
 
 #Installing ruby
+echo "installing ruby"
 wget http://rubyforge.org/frs/download.php/71096/ruby-enterprise-1.8.7-2010.02.tar.gz
-tar xzvf ruby-enterprise-1.8.7-2010.02.tar.gz
+tar xzf ruby-enterprise-1.8.7-2010.02.tar.gz
 ./ruby-enterprise-1.8.7-2010.02/installer -a /opt/ruby --no-dev-docs --dont-install-useful-gems
 echo 'PATH=$PATH:/opt/ruby/bin/'> /etc/profile.d/rubyenterprise.sh
 rm -rf ./ruby-enterprise-1.8.7-2010.02/
 rm ruby-enterprise-1.8.7-2010.02.tar.gz
+echo "completed installating ruby"
 
 #Installing chef & Puppet
+echo "installing chef and puppet"
 /opt/ruby/bin/gem install chef --no-ri --no-rdoc
 /opt/ruby/bin/gem install puppet --no-ri --no-rdoc
+echo "completed installing chef and puppet"
 
 #Installing vagrant keys
+echo "installing vagrant keys"
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
-
-#Installing the virtualbox guest additions
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
-mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-
-rm VBoxGuestAdditions_$VBOX_VERSION.iso
+echo "completed installing vagrant keys"
 
 # Remove items used for building, since they aren't needed anymore
-apt-get -y remove linux-headers-$(uname -r) build-essential
+echo "removing development packages"
+apt-get -y remove linux-kernel-devel linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
+echo "completed removing development packages"
 
 # Not sure if this is needed for hardy too
 # Removing leftover leases and persistent rules
